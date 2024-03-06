e7acc1dda47b8ed8de9daa244ec26b4c976382d6
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-03-06 09:52:54 -0300

    Teste

--- Tentativa.c
+++ Tentativa.c
@@ -11,243 +11,235 @@ typedef struct Clock {
     int p[CLOCK_SIZE];
 } Clock;
 
-typedef struct Message {
+typedef struct Task {
     int pid;
     Clock clock;
-    int source;
-} Message;
+} Task;
 
-typedef struct Task{
+typedef struct ThreadInputArgs {
     int pid;
-    Clock received;
-}
+    Clock* clock;
+    int source;
+} ThreadInputArgs;
 
-typedef struct Queue{
+typedef struct ThreadOutputArgs {
+    Task task;
+} ThreadOutputArgs;
+
+typedef struct Queue {
     Task t[MAX_QUEUE];
     int front, rear, size;
     pthread_mutex_t lock;
     pthread_cond_t notFull, notEmpty;
-}
+} Queue;
 
-Clock process_clock;
+typedef struct ThreadProcessArgs {
+    Queue *input;
+    Queue *output;
+} ThreadProcessArgs;
+
 Queue input_queue;
 Queue output_queue;
 
-void Event(int pid, Clock *clock){
-    clock->p[pid]++;
-}
-
-void create_task(Clock *clock, int pid, Task *newTask){
-    newTask.pid = pid;
-    for(int i = 0; i < CLOCK_SIZE; i++){
-        newTask.received.p[i] = clock->p[i];
-    }
-}
-
-void initQueue(Queue *q){
-    q-> front = q->rear = q->size = 0;
+void initQueue(Queue *q) {
+    q->front = 0;
+    q->rear = 0;
+    q->size = 0;
     pthread_mutex_init(&q->lock, NULL);
     pthread_cond_init(&q->notFull, NULL);
     pthread_cond_init(&q->notEmpty, NULL);
 }
 
-void enqueue(Queue *q, Task item){
+void Event(int pid, Clock *clock) {
+    clock->p[pid]++;
+}
+
+void enqueue(Queue *q, Task t) {
     pthread_mutex_lock(&q->lock);
-    while(q->size == MAX_QUEUE){
+    while (q->size == MAX_QUEUE) {
         pthread_cond_wait(&q->notFull, &q->lock);
     }
-    q->t[q->rear] = item;
+    q->t[q->rear] = t;
     q->rear = (q->rear + 1) % MAX_QUEUE;
     q->size++;
     pthread_cond_signal(&q->notEmpty);
     pthread_mutex_unlock(&q->lock);
 }
 
-void create_and_enqueue_task(int pid) {
-    Task newTask;
-    create_task(&process_clock, pid, &newTask);
-    enqueue(&output_queue, newTask);
-}
-
-void dequeue(Queue *q, Task *item){
+void dequeue(Queue *q, Task *t) {
     pthread_mutex_lock(&q->lock);
-    while(q->size == 0){
+    while (q->size == 0) {
         pthread_cond_wait(&q->notEmpty, &q->lock);
     }
-    *item = q->t[q->front];
+    *t = q->t[q->front];
     q->front = (q->front + 1) % MAX_QUEUE;
     q->size--;
     pthread_cond_signal(&q->notFull);
     pthread_mutex_unlock(&q->lock);
 }
 
-void process_task(Task *task){
-    printf("Task recebida: %d %d %d\n",  task->received.p[0], task->received.p[1], task->received.p[2]);
-    printf("Relogio do processo: %d %d %d\n", process_clock.p[0], process_clock.p[1], process_clock.p[2]);
-    printf("Processando task...\n");
-    sleep(2);
-    for(int i = 0; i < CLOCK_SIZE; i++){
-        if(task->received.p[i] > process_clock.p[i]){
-            process_clock.p[i] = task->received.p[i];
-        }
-    }
-    process_clock.p[task->pid]++;
-    printf("Relogio do processo atualizado: %d %d %d\n", process_clock.p[0], process_clock.p[1], process_clock.p[2]);
-}
-
-void send_message(int pid, int dest, Clock *clock){
-    MPI_Send(&clock, sizeof(Clock), MPI_BYTE, dest, 0, MPI_COMM_WORLD);
-    printf("Processo %d enviou para o processo %d: Clock(%d, %d, %d)\n", pid, dest, clock->p[0], clock->p[1], clock->p[2]));
-}
-
-void receive_message(int pid, Clock *clock, int source){
+Clock Receive(int pid, Clock *clock, int source) {
     Clock received;
     MPI_Recv(&received, sizeof(Clock), MPI_BYTE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for(int i = 0; i < CLOCK_SIZE; i++){
-        if(received.p[i] > clock->p[i]){
+    for (int i = 0; i < CLOCK_SIZE; i++) {
+        if (received.p[i] > clock->p[i]) {
             clock->p[i] = received.p[i];
         }
     }
+    clock->p[pid]++;
     printf("Processo %d recebeu do processo %d: Clock(%d, %d, %d)\n", pid, source, clock->p[0], clock->p[1], clock->p[2]);
+    return *clock;
 }
 
-void* input_thread(void *arg) {
-    int pid = *((int*)arg);
-    int source = *((int*)arg + 1);
+void Send(int pid, Clock *clock, int dest) {
+    clock->p[pid]++;
+    MPI_Send(clock, sizeof(Clock), MPI_BYTE, dest, 0, MPI_COMM_WORLD);
+    printf("Processo %d enviou para o processo %d: Clock(%d, %d, %d)\n", pid, dest, clock->p[0], clock->p[1], clock->p[2]);
+}
+
+void create_input_task(int pid, Clock *clock) {
+    Task t;
+    t.pid = pid;
+    t.clock = *clock;
+    enqueue(&input_queue, t);
+}
+
+void create_output_task(int pid, Clock *clock) {
+    Task t;
+    t.pid = pid;
+    t.clock = *clock;
+    enqueue(&output_queue, t);
+}
+
+void *input_thread(void *arg) {
+    ThreadInputArgs *InputArgs = (ThreadInputArgs *)arg;
+    int pid = InputArgs->pid;
+    Clock *clock = InputArgs->clock;
+    int source = InputArgs->source;
+    sleep(1);
+    Event(pid, clock);
+    Send(pid, clock, 1); 
+    Clock received_clock = Receive(pid, clock, 1);
+    create_input_task(pid, &received_clock);
+
+    free(InputArgs);
+    return NULL;
+}
+
+void *process_thread(void *arg) {
+    ThreadProcessArgs *ProcessArgs = (ThreadProcessArgs*)arg;
+    Queue *input_queue = ProcessArgs->input;
+    Queue *output_queue = ProcessArgs->output;
+    Task t;
     while (1) {
-        Clock received_clock;
-        MPI_Recv(&received_clock, sizeof(Clock), MPI_BYTE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        Task newTask;
-        create_task(&received_clock, pid, &newTask);
-        enqueue(&input_queue, newTask);
+        dequeue(input_queue, &t);
+        Event(t.pid, &t.clock);
+        printf("Processo %d executou: Clock(%d, %d, %d)\n", t.pid, t.clock.p[0], t.clock.p[1], t.clock.p[2]);
+        sleep(2);
+        create_output_task(t.pid, &t.clock);
     }
     return NULL;
 }
 
-void* process_thread(void *arg) {
-    int pid = *((int*)arg);
-    int *thread_args = (int*)arg;
-    while (1) {
-        Task task;
-        dequeue(&input_queue, &task);
-        printf("Processo %d processou uma task.\n", pid);
-        process_task(&task);
-        create_and_enqueue_task(pid);
-    }
+void *output_thread(void *arg) {
+    ThreadOutputArgs *OutputArgs = (ThreadOutputArgs *)arg;
+    Task t = OutputArgs->task;
+    sleep(2);
+    Send(t.pid, &t.clock, t.pid == 0 ? 1 : 0);
+    free(OutputArgs);
     return NULL;
 }
 
-void* output_thread(void *arg) {
-    int *thread_args = (int*)arg;
-    int pid = thread_args[0];
-    int dest = thread_args[1];
-    while (1) {
-        Task task;
-        dequeue(&output_queue, &task);
-        printf("Processo %d enviou mensagem MPI com base em uma task.\n", pid);
-        send_message(pid, dest, &task.received);
-    }
-    return NULL;
+void process0(int pid) {
+    Clock clock = {0, 0, 0};
+    initQueue(&input_queue);
+    initQueue(&output_queue);
+
+    ThreadInputArgs *InputArgs = malloc(sizeof(ThreadInputArgs));
+    InputArgs->pid = pid;
+    InputArgs->clock = &clock;
+    InputArgs->source = 1;
+
+    ThreadProcessArgs ProcessArgs;
+    ProcessArgs.input = &input_queue;
+    ProcessArgs.output = &output_queue;
+
+    pthread_t input, process, output;
+    pthread_create(&input, NULL, input_thread, InputArgs);
+    pthread_create(&process, NULL, process_thread, &ProcessArgs);
+    pthread_create(&output, NULL, output_thread, NULL);
+
+    pthread_join(input, NULL);
+    pthread_join(process, NULL);
+    pthread_join(output, NULL);
+
+    free(InputArgs);
 }
 
-void process0(){
-    int my_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    pthread_t input_thread, process_thread, output_thread;
-    Clock clock = {{0, 0, 0}};
+void process1(int pid) {
+    Clock clock = {0, 0, 0};
+    initQueue(&input_queue);
+    initQueue(&output_queue);
 
-    int thread_args[] = {my_rank, 1};  // Adjust destination accordingly
-    pthread_create(&input_thread, NULL, input_thread, thread_args);
+    ThreadInputArgs *InputArgs = malloc(sizeof(ThreadInputArgs));
+    InputArgs->pid = pid;
+    InputArgs->clock = &clock;
+    InputArgs->source = 0;
 
-    int process_args[] = {my_rank};
-    pthread_create(&process_thread, NULL, process_thread, process_args);
+    pthread_t input, process, output;
+    pthread_create(&input, NULL, input_thread, InputArgs);
 
-    int output_args[] = {my_rank, 1};  // Adjust destination accordingly
-    pthread_create(&output_thread, NULL, output_thread, output_args);
+    ThreadProcessArgs ProcessArgs;
+    ProcessArgs.input = &input_queue;
+    ProcessArgs.output = &output_queue;
+    pthread_create(&process, NULL, process_thread, &ProcessArgs);
+    pthread_create(&output, NULL, output_thread, NULL);
 
-    // Operações do processo 0
-    send_message(0, &clock, 1);
-    receive_message(0, &clock, 1);
+    pthread_join(input, NULL);
+    pthread_join(process, NULL);
+    pthread_join(output, NULL);
 
-    send_message(0, &clock, 2);
-    receive_message(0, &clock, 2);
-
-    send_message(0, &clock, 1);
-    receive_message(0, &clock, 1);
-
-    printf("Processo %d, Clock troca com o processo 1: (%d, %d, %d)\n", my_rank, clock.p[0], clock.p[1], clock.p[2]);
-
-    // Aguardar o término das threads (o exemplo usa um loop infinito para simulação)
-    pthread_join(input_thread, NULL);
-    pthread_join(process_thread, NULL);
-    pthread_join(output_thread, NULL);
-    
+    free(InputArgs);
 }
 
-void process1(){
-    int my_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    pthread_t input_thread, process_thread, output_thread;
-    Clock clock = {{0, 0, 0}};
+void process2(int pid) {
+    Clock clock = {0, 0, 0};
+    initQueue(&input_queue);
+    initQueue(&output_queue);
 
-    int thread_args[] = {my_rank, 0};  // Adjust destination accordingly
-    pthread_create(&input_thread, NULL, input_thread, thread_args);
+    ThreadInputArgs *InputArgs = malloc(sizeof(ThreadInputArgs));
+    InputArgs->pid = pid;
+    InputArgs->clock = &clock;
+    InputArgs->source = 0;
 
-    int process_args[] = {my_rank};
-    pthread_create(&process_thread, NULL, process_thread, process_args);
+    pthread_t input, process, output;
+    pthread_create(&input, NULL, input_thread, InputArgs);
 
-    int output_args[] = {my_rank, 0};  // Adjust destination accordingly
-    pthread_create(&output_thread, NULL, output_thread, output_args);
+    ThreadProcessArgs ProcessArgs;
+    ProcessArgs.input = &input_queue;
+    ProcessArgs.output = &output_queue;
+    pthread_create(&process, NULL, process_thread, &ProcessArgs);
+    pthread_create(&output, NULL, output_thread, NULL);
 
-    // Operações do processo 1
-    send_message(1, &clock, 0);
-    receive_message(1, &clock, 0);
+    pthread_join(input, NULL);
+    pthread_join(process, NULL);
+    pthread_join(output, NULL);
 
-    receive_message(1, &clock, 0);
-
-    // Aguardar o término das threads (o exemplo usa um loop infinito para simulação)
-    pthread_join(input_thread, NULL);
-    pthread_join(process_thread, NULL);
-    pthread_join(output_thread, NULL);
-}
-
-void process2(){
-    int my_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    pthread_t input_thread, process_thread, output_thread;
-    Clock clock = {{0, 0, 0}};
-
-    int thread_args[] = {my_rank, 0};  // Adjust destination accordingly
-    pthread_create(&input_thread, NULL, input_thread, thread_args);
-
-    int process_args[] = {my_rank};
-    pthread_create(&process_thread, NULL, process_thread, process_args);
-
-    int output_args[] = {my_rank, 0};  // Adjust destination accordingly
-    pthread_create(&output_thread, NULL, output_thread, output_args);
-
-    // Operações do processo 2
-    send_message(2, &clock, 0);
-    receive_message(2, &clock, 0);
-
-    // Aguardar o término das threads (o exemplo usa um loop infinito para simulação)
-    pthread_join(input_thread, NULL);
-    pthread_join(process_thread, NULL);
-    pthread_join(output_thread, NULL);
+    free(InputArgs);
 }
 
 int main(void) {
     int my_rank;
-    MPI_Init(NULL, NULL);
+    MPI_Init_thread(NULL, NULL, MPI_THREAD_SERIALIZED, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if (my_rank == 0) {
-        process0();
+        process0(my_rank);
     } else if (my_rank == 1) {
-        process1();
+        process1(my_rank);
     } else if (my_rank == 2) {
-        process2();
+        process2(my_rank);
     }
-    MPI_Finalize();
+
     return 0;
 }
