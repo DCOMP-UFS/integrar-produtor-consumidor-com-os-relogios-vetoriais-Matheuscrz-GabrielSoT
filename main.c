faef179478d815bdf17693f5ea4f9d80ff9415b9
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-03-14 21:16:32 -0300

    Implementação. Obs: Está entrando em deadlook

--- main.c
+++ main.c
@@ -8,31 +8,27 @@
 
 #define MAX_QUEUE 3
 #define CLOCK_SIZE 3
+#define MESSAGE_SIZE 100
 
-/**
- * @brief Estrutura que representa um relógio vetorial
- * 
- */
 typedef struct Clock {
     int p[CLOCK_SIZE];
 } Clock;
 
-/**
- * @brief Estrutura que representa uma tarefa
- * 
- */
-typedef struct Task {
-    int pid;
-    Clock clock;
-    int source;
-} Task;
+typedef enum {
+    EVENT,
+    SEND,
+    RECEIVE
+} MessageType;
 
-/**
- * @brief Estrutura que representa uma fila de tarefas
- * 
- */
+typedef struct QueueItem {
+    int pid;
+    int target;
+    MessageType type;
+    char message[MESSAGE_SIZE];
+} QueueItem;
+
 typedef struct Queue {
-    Task t[MAX_QUEUE];
+    QueueItem items[MAX_QUEUE];
     int front, rear, size;
     pthread_mutex_t lock;
     pthread_cond_t notFull, notEmpty;
@@ -41,11 +37,6 @@ typedef struct Queue {
 Queue input_queue;
 Queue output_queue;
 
-/**
- * @brief Método que inicializa uma fila
- * 
- * @param q - Ponteiro para a fila
- */
 void initQueue(Queue *q) {
     q->front = 0;
     q->rear = 0;
@@ -55,256 +46,205 @@ void initQueue(Queue *q) {
     pthread_cond_init(&q->notEmpty, NULL);
 }
 
-/**
- * @brief - Método que incrementa o relógio vetorial
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- */
-void Event(int pid, Clock *clock) {
-    clock->p[pid]++;
-}
-
-/**
- * @brief Método que insere uma tarefa na fila
- * 
- * @param q - Ponteiro para a fila
- * @param t - Tarefa a ser inserida
- */
-void enqueue(Queue *q, Task t) {
+void enqueue(Queue *q, QueueItem item) {
     pthread_mutex_lock(&q->lock);
+    printf("Enqueue: Lock adquirido\n");
     while (q->size == MAX_QUEUE) {
+        printf("Enqueue: Fila cheia, aguardando não cheia...\n");
         pthread_cond_wait(&q->notFull, &q->lock);
     }
-    q->t[q->rear] = t;
+    printf("Enqueue: Espaço disponível na fila, inserindo item...\n");
+    q->items[q->rear] = item;
     q->rear = (q->rear + 1) % MAX_QUEUE;
     q->size++;
     pthread_cond_signal(&q->notEmpty);
     pthread_mutex_unlock(&q->lock);
+    printf("Enqueue: Lock liberado\n");
 }
 
-/**
- * @brief Método que remove uma tarefa da fila
- * 
- * @param q - Ponteiro para a fila
- * @param t - Ponteiro para a tarefa a ser removida
- */
-void dequeue(Queue *q, Task *t) {
+QueueItem dequeue(Queue *q) {
     pthread_mutex_lock(&q->lock);
+    printf("Dequeue: Lock adquirido\n");
     while (q->size == 0) {
+        printf("Dequeue: Fila vazia, aguardando não vazia...\n");
         pthread_cond_wait(&q->notEmpty, &q->lock);
     }
-    *t = q->t[q->front];
+    printf("Dequeue: Item disponível na fila, removendo item...\n");
+    QueueItem item = q->items[q->front];
     q->front = (q->front + 1) % MAX_QUEUE;
     q->size--;
     pthread_cond_signal(&q->notFull);
     pthread_mutex_unlock(&q->lock);
+    printf("Dequeue: Lock liberado\n");
+    return item;
 }
 
-/**
- * @brief - Método que recebe uma mensagem e insere na fila
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- * @param source - Identificador do processo que enviou a mensagem
- */
-void ReceiveAndEnqueue(int pid, Clock *clock, int source) {
-    Clock received;
-    MPI_Recv(&received, sizeof(Clock), MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    printf("Processo %d recebeu do processo %d: Clock(%d, %d, %d)\n", pid, source, received.p[0], received.p[1], received.p[2]);
-    Task newTask = {pid, received, source};
-    enqueue(&input_queue, newTask);
+char* Increment(int pid, Clock *clock) {
+    clock->p[pid]++;
+    char* message = malloc(MESSAGE_SIZE * sizeof(char)); // Aloca memória para a mensagem
+    sprintf(message, "Processo %d: Clock(%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]); // Cria um ponteiro para a mensagem
+    return message; // Retorna um ponteiro para a mensagem
 }
 
-/**
- * @brief - Método que processa uma mensagem e insere na fila
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- */
-void ProcessAndEnqueue(int pid, Clock *clock) {
-    Task currentTask;
-    dequeue(&input_queue, &currentTask);
+void updateClock(Clock *clock, Clock *received) {
     for (int i = 0; i < CLOCK_SIZE; i++) {
-        if (currentTask.clock.p[i] > clock->p[i]) {
-            clock->p[i] = currentTask.clock.p[i];
+        if (received->p[i] > clock->p[i]) {
+            clock->p[i] = received->p[i];
         }
     }
+}
+
+char* Send_MPI(int pid, Clock *clock, int dest){
     clock->p[pid]++;
-    printf("Processo %d recebeu do processo %d: Clock(%d, %d, %d)\n", pid, currentTask.source, clock->p[0], clock->p[1], clock->p[2]);
-    Task nextTask = {pid, *clock, currentTask.source};
-    enqueue(&output_queue, nextTask);
-}
-
-/**
- * @brief - Método que incrementa o relógio vetorial e imprime o valor
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- */
-void ProcessEvent(int pid, Clock *clock) {
-    Event(pid, clock);
-    printf("Processo %d: Clock(%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]);
-}
-
-/**
- * @brief Método que cria uma tarefa e insere na fila
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- * @param dest - Identificador do processo de destino
- */
-void createAndEnqueueTask(int pid, Clock *clock, int dest) {
-    Task newTask = {pid, *clock, dest};
-    enqueue(&output_queue, newTask);
-}
-
-/**
- * @brief Método que envia uma mensagem para outro processo
- * 
- * @param pid - Identificador do processo
- * @param clock - Ponteiro para o relógio vetorial
- * @param dest - Identificador do processo de destino
- */
-void SendFromOutputQueue(int pid, Clock *clock, int dest) {
-    Task currentTask;
-    dequeue(&output_queue, &currentTask);
-    MPI_Send(&currentTask.clock, sizeof(Clock), MPI_BYTE, dest, 0 , MPI_COMM_WORLD);
-    printf("Processo %d enviou para o processo %d: Clock(%d, %d, %d)\n", pid, dest, currentTask.clock.p[0], currentTask.clock.p[1], currentTask.clock.p[2]);
-}
-
-/**
- * @brief Método que usa uma thread para receber mensagens
- * 
- * @param arg - Identificador do processo
- * @return void* 
- */
-void* input_thread(void* arg) {
-    int pid = *((int*)arg);
-    Clock clock = {0, 0, 0};
-    ReceiveAndEnqueue(pid, &clock, pid);
-    return NULL;
-}
-
-/**
- * @brief Método que usa uma thread para processar mensagens
- * 
- * @param arg - Identificador do processo
- * @return void* 
- */
-void* process_thread(void* arg) {
-    int pid = *((int*)arg);
-    Clock clock = {0, 0, 0};
-    ProcessAndEnqueue(pid, &clock);
-    return NULL;
-}
-
-/**
- * @brief Método que usa uma thread para enviar mensagens
- * 
- * @param arg - Identificador do processo
- * @return void* 
- */
-void* output_thread(void* arg) {
-    int pid = *((int*)arg);
-    Clock clock = {0, 0, 0};
-    SendFromOutputQueue(pid, &clock, (pid + 1) % 3);
-    return NULL;
-}
-
-/**
- * @brief Método que simula o comportamento do processo 0
- * 
- */
-void process0(){
-    Clock clock = {{0, 0, 0}};
-    ProcessEvent(0, &clock);
-    printf("Processo: %d, Clock: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
-    createAndEnqueueTask(0, &clock, 1);
-    SendFromOutputQueue(0, &clock, 1);
-    ReceiveAndEnqueue(0, &clock, 1);
-    ProcessAndEnqueue(0, &clock);
-    SendFromOutputQueue(0, &clock, 2);
-    ReceiveAndEnqueue(0, &clock, 2);
-    ProcessAndEnqueue(0, &clock);
-    SendFromOutputQueue(0, &clock, 1);
-    ProcessEvent(0, &clock);
-    printf("Processo %d, Clock troca com o processo 1: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
-}
-
-/**
- * @brief Método que simula o comportamento do processo 1
- * 
- */
-void process1(){
-    Clock clock = {{0, 0, 0}};
-    printf("Processo: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
-    createAndEnqueueTask(1, &clock, 0);
-    SendFromOutputQueue(1, &clock, 0);
-    ReceiveAndEnqueue(1, &clock, 0);
-    ProcessAndEnqueue(1, &clock);
-    ReceiveAndEnqueue(1, &clock, 0);
-    ProcessAndEnqueue(1, &clock);
-}
-
-/**
- * @brief Método que simula o comportamento do processo 2
- * 
- */
-void process2(){
-    Clock clock = {{0, 0, 0}};
-    ProcessEvent(2, &clock);
-    printf("Processo: %d, Clock: (%d, %d, %d)\n", 2, clock.p[0], clock.p[1], clock.p[2]);
-    createAndEnqueueTask(2, &clock, 0);
-    SendFromOutputQueue(2, &clock, 0);
-    ReceiveAndEnqueue(2, &clock, 0);
-    ProcessAndEnqueue(2, &clock);
-}
-
-/**
- * @brief Método principal
- * 
- * @param argc - Número de argumentos
- * @param argv - Vetor de argumentos
- * @return int 
- */
-int main(int argc, char *argv[]) {
-    int provide; // Armazena o nível de suporte a threads do MPI
-    int rank;    // Identificador do processo
-
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provide); // Inicializa o ambiente MPI
-    // Verifica se o ambiente MPI foi inicializado corretamente
-    if (provide != MPI_THREAD_MULTIPLE) {
-        printf("Error: MPI_THREAD_MULTIPLE não suportado\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
+    int error = MPI_Send(clock, sizeof(Clock), MPI_BYTE, dest, 0, MPI_COMM_WORLD);
+    if (error != MPI_SUCCESS) {
+        fprintf(stderr, "Falha ao enviar mensagem MPI\n");
+        MPI_Abort(MPI_COMM_WORLD, error);
     }
+    char* message = malloc(MESSAGE_SIZE * sizeof(char)); // Aloca memória para a mensagem
+    sprintf(message, "Processo %d enviou para o processo %d: Clock(%d, %d, %d)\n", pid, dest, clock->p[0], clock->p[1], clock->p[2]); // Cria um ponteiro para a mensagem
+    return message; // Retorna um ponteiro para a mensagem
+}
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+char* Recv_MPI(int pid, Clock *clock, int source){
+    Clock received;
+    int error = MPI_Recv(&received, sizeof(Clock), MPI_BYTE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (error != MPI_SUCCESS) {
+        fprintf(stderr, "Falha ao receber mensagem MPI\n");
+        MPI_Abort(MPI_COMM_WORLD, error);
+    }
+    updateClock(clock, &received);
+    char* message = malloc(MESSAGE_SIZE * sizeof(char)); // Aloca memória para a mensagem
+    sprintf(message, "Processo %d recebeu do processo %d: Clock(%d, %d, %d)\n", pid, source, received.p[0], received.p[1], received.p[2]); // Cria um ponteiro para a mensagem
+    return message; // Retorna um ponteiro para a mensagem
+}
+
+void* processThread(void *arg) {
+    Clock clock = {{0, 0, 0}};
+    while(1){
+        QueueItem item = dequeue(&input_queue);
+        if(item.type == EVENT){
+            char* message = Increment(item.pid, &clock);
+            strcpy(item.message, message); // Copia a mensagem para o item
+        } else if(item.type == SEND){
+            char* message = Send_MPI(item.pid, &clock, item.target);
+            strcpy(item.message, message); // Copia a mensagem para o item
+        } else if(item.type == RECEIVE){
+            char* message = Recv_MPI(item.pid, &clock, item.target); // Cria um ponteiro para a mensagem 
+            strcpy(item.message, message); // Copia a mensagem para o item
+        }
+        printf("Output - Fila");
+        enqueue(&output_queue, item);
+        if (input_queue.size == 0 && item.type != EVENT) {
+            break;
+        }
+    }
+    return NULL;
+}
+
+void* inputThread(void *arg) {
+    QueueItem *item = (QueueItem*)arg;
+    printf("Input - Fila");
+    enqueue(&input_queue, *item); // Adiciona o item na fila de entrada
+    free(item); // Libera a memória alocada
+    return NULL;
+}
+
+void* outputThread(void *arg) {
+    while(1) {
+        QueueItem item = dequeue(&output_queue);
+        if(item.message != NULL){
+            printf("%s", item.message);
+            // free(item.message);
+        } else {
+            break;
+        }
+    }
+    return NULL;
+}
+
+void Event(int pid){
+    MessageType type = EVENT;
+    QueueItem *item = malloc(sizeof(QueueItem));
+    item->pid = pid;
+    item->type = type;
+    pthread_t thread;
+    pthread_create(&thread, NULL, inputThread, item);
+    pthread_join(thread, NULL);
+}
+
+void Send(int pid, int dest){
+    MessageType type = SEND;
+    QueueItem *item = malloc(sizeof(QueueItem));
+    item->pid = pid;
+    item->type = type;
+    item->target = dest;
+    pthread_t thread;
+    pthread_create(&thread, NULL, inputThread, item);
+    pthread_join(thread, NULL);
+}
+
+void Receive(int pid, int source){
+    MessageType type = RECEIVE;
+    QueueItem *item = malloc(sizeof(QueueItem));
+    item->pid = pid;
+    item->type = type;
+    item->target = source;
+    pthread_t thread;
+    pthread_create(&thread, NULL, inputThread, item);
+    pthread_join(thread, NULL);
+}
+
+void process0(){
+    Event(0);
+    Send(0, 1);
+    Receive(0, 1);
+    Send(0, 2);
+    Receive(0, 2);
+    Send(0, 1);
+    Event(0);
+}
+
+void process1(){ 
+    Send(1, 0);
+    Receive(1, 0);
+    Receive(1, 0);
+}
+
+void process2(){ 
+    Event(2);
+    Send(2, 0);
+    Receive(2, 0);
+}
+
+int main() {
+    int my_rank;
+    int error = MPI_Init(NULL, NULL);
+    if (error != MPI_SUCCESS) {
+        fprintf(stderr, "Falha ao inicializar MPI\n");
+        MPI_Abort(MPI_COMM_WORLD, error);
+    }
+    error = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    if (error != MPI_SUCCESS) {
+        fprintf(stderr, "Falha ao obter o rank MPI\n");
+        MPI_Abort(MPI_COMM_WORLD, error);
+    }
 
     initQueue(&input_queue);
     initQueue(&output_queue);
+    pthread_t process, output;
+    pthread_create(&process, NULL, processThread, NULL);
+    pthread_create(&output, NULL, outputThread, NULL);
 
-    int arg1 = rank;
-    int arg2 = rank;
-    int arg3 = rank;
-
-    pthread_t input, process, output;
-    pthread_create(&input, NULL, input_thread, &arg1);
-    pthread_create(&process, NULL, process_thread, &arg2);
-    pthread_create(&output, NULL, output_thread, &arg3);
-
-    if(rank == 0) {
+    if(my_rank == 0){
         process0();
-    } else if(rank == 1) {
+    } else if(my_rank == 1){
         process1();
-    } else if(rank == 2) {
+    } else if(my_rank == 2){
         process2();
     }
-
-    pthread_join(input, NULL);
     pthread_join(process, NULL);
     pthread_join(output, NULL);
-
     MPI_Finalize();
     return 0;
 }
